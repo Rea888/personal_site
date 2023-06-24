ececbf0b570eb2dcb82fe9306b5a7a33627a18a9
rea <m.rakhely.viktoria@gmail.com>
2023-06-24 19:48:43 +0200

    Changed update start button event listener in game in main.js

--- public/import/assets/js/main.js
+++ public/import/assets/js/main.js
@@ -650,12 +650,17 @@
 
         startButton.addEventListener('click', function () {
 
-            generateStars();
-            canvas.style.display = 'block';
-            startButton.style.display = 'none';
-            restartButton.style.display = 'block';
-            initializeGame();
 
+            if (window.innerWidth <= 768 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                // If on mobile or tablet, redirect to the game page
+                window.location.href = 'game';
+            } else {
+                generateStars();
+                canvas.style.display = 'block';
+                startButton.style.display = 'none';
+                restartButton.style.display = 'block';
+                initializeGame();
+            }
         });
 
         restartButton.addEventListener('click', function () {
