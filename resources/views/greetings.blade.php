371fa39b56ee6e66982dfd5ade7887f236984e8e
rea <m.rakhely.viktoria@gmail.com>
2023-07-14 15:37:07 +0200

    changed image in greetings blade

--- resources/views/greetings.blade.php
+++ resources/views/greetings.blade.php
@@ -353,7 +353,7 @@
 
     <!-- ======= Portfolio Section ======= -->
     <div id="portfolio" class="paddsection"
-         style="display:block; background-image: url('{{ env('BASE_URL') . env('VERSION_HASH') }}/assets/img/oop.PNG');">
+         style="display:block; background-image: url('{{ env('BASE_URL') . env('VERSION_HASH') }}/assets/img/wavesNegative');">
         <div class="container-fluid">
             <div class="col-lg-12 text-center">
                 <div class="descr">
